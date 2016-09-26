05a61fb8eacac742a1f782699b512ffe19a3054d
aliang1122 <aliang1122@hotmail.com>
2016-09-26 21:09:15 +0800

    menu test

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var app = require('express')(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    wechat=require('./lib/wechat'),
+    conf=require('./lib/config');
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app.use
+
+wechat(conf.wechat);
+
+wechat.createMenu(require('./lib/menu.json'));
+
 app.use('/wxapi',require('./lib/routers/wxapi.js'));
 
 app.use('/*',function(req,res,next){
